a439b7a36d14caf1c8edf3f98cb718ab855ce34c
Shubhamtripathi095 <122169655+Shubhamtripathi095@users.noreply.github.com>
2026-06-11 21:38:28 +0530

    Update gemini_service.py

--- services/gemini_service.py
+++ services/gemini_service.py
@@ -5,7 +5,46 @@ client = genai.Client(
     api_key=st.secrets["GEMINI_API_KEY"]
 )
 
-def generate(prompt):
+def generate_ba_package(requirement):
+
+    prompt = f"""
+You are a Senior BFSI Business Analyst.
+
+Requirement:
+{requirement}
+
+Generate detailed documentation.
+
+# BRD
+Business Objective
+Business Problem
+Scope
+Stakeholders
+Assumptions
+Risks
+
+# FRD
+Functional Requirements
+
+# SRS
+Functional Requirements
+Non Functional Requirements
+
+# EPICS
+
+# USER STORIES
+
+# ACCEPTANCE CRITERIA
+
+# GHERKIN
+
+# TEST CASES
+
+# PROCESS FLOW
+
+Provide detailed enterprise-grade output.
+"""
+
     response = client.models.generate_content(
         model="gemini-2.5-flash",
         contents=prompt
